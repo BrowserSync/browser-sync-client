6d2d008c8e2b469e940ea6fbefc9cca4a5b04b58
Shane Osbourne <shane.osbourne8@gmail.com>
2014-03-30 21:59:08 +0100

    Add uglify option

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-var fs = require("fs");
-var path = require("path");
+"use strict";
 
-var clientScript = path.resolve(__dirname + "/lib/browser-sync-client.js");
-var shims = path.resolve(__dirname + "/lib/client-shims.js");
+var fs       = require("fs");
+var path     = require("path");
+
+var script = path.resolve(__dirname + "/lib/browser-sync-client.js");
+var shims  = path.resolve(__dirname + "/lib/client-shims.js");
 
 module.exports.middleware = function () {
 
-    var jsFile    = fs.readFileSync(clientScript);
-    var jsShims   = fs.readFileSync(shims);
-    var result    = jsShims + jsFile;
+    return function (options) {
 
-    return function (req, res) {
-        res.setHeader("Content-Type", "text/javascript");
-        res.end(result);
-    };
+        var jsFile  = fs.readFileSync(script);
+        var jsShims = fs.readFileSync(shims);
+        var result  = jsShims + jsFile;
+
+        if (options && options.minify) {
+            var UglifyJS = require("uglify-js");
+            result = UglifyJS.minify(result, {fromString: true});
+        }
+
+        return function (req, res) {
+            res.setHeader("Content-Type", "text/javascript");
+            res.end(result);
+        };
+    }
 };
