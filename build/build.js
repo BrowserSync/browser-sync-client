2d4fb8b8430e564fd203bdd98eb515af43a711f7
Shane Osbourne <shane.osbourne8@gmail.com>
2014-03-31 22:28:06 +0100

    Add build for events + browserify

--- build/build.js
+++ build/build.js
@@ -1,22 +1,47 @@
 var UglifyJS    = require("uglify-js");
-var fs          = require("vinyl-fs");
+var fs         = require("vinyl-fs");
 var through2    = require("through2");
 var File        = require("vinyl");
 var browserify  = require('browserify');
 
+var eventsFile  = fs.src("lib/events.js");
+
+/**
+ * @returns {*}
+ */
 var uglifier = function () {
-    var combined = "";
     return through2.obj(function (file, enc, cb) {
-        combined += file.toString();
-        cb();
-    }, function (cb) {
-        var minified = UglifyJS.minify(combined, {fromString: true});
+        var minified = UglifyJS.minify(file.toString(), {fromString: true});
         this.push(new File({
             cwd:  "./",
             base: "./",
             path: "./dist.js",
             contents: new Buffer(minified.code)
         }));
+        cb(null);
+    });
+};
+
+/**
+ *
+ * @returns {*}
+ */
+var combiner = function (events) {
+
+    var eventCode;
+    events.pipe(through2.obj(function (file, enc, cb) {
+        eventCode = file.contents.toString();
+        cb();
+    }));
+    var string;
+    return through2.obj(function (data, enc, cb) {
+        string += data.toString();
+        cb(null);
+    }, function (cb) {
+        if (string.length) {
+            this.push(new Buffer(eventCode + string));
+        }
+        this.push(null);
         cb();
     });
 };
@@ -24,5 +49,6 @@ var uglifier = function () {
 var b = browserify();
 b.add('./lib/index.js');
 b.bundle()
+    .pipe(combiner(eventsFile))
     .pipe(uglifier())
     .pipe(fs.dest("./dist"));
