e66c591908304c5e4741d7df5367df1b9b73626e
Shane Osbourne <shane.osbourne8@gmail.com>
2014-04-13 19:39:43 +0100

    add specs for utils

--- test/client-new/utils.js
+++ test/client-new/utils.js
@@ -39,76 +39,73 @@ describe("Generic Browser utils", function(){
             assert.equal(actual.y, 400);
         });
     });
+    describe("getting a single element", function(){
+        var fakeElems = [
+            {
+                id: "item1",
+                tagName: "link"
+            },
+            {
+                id: "item2",
+                tagName: "link"
+            }
+        ], stub;
+        before(function(){
+            stub = sinon.stub(document, "getElementsByTagName").returns(fakeElems);
+        });
+        after(function () {
+            stub.restore();
+        });
+        it("should call getElementsByTagName() with the tagname", function(){
+            browserUtils.utils.getSingleElement("link", 0);
+            sinon.assert.calledWithExactly(stub, "link");
+        });
+        it("should return the correct index: 1", function(){
+            var actual   = browserUtils.utils.getSingleElement("link", 0);
+            var expected = "item1";
+            assert.equal(actual.id, expected);
+        });
+        it("should return the correct index: 2", function(){
+            var actual   = browserUtils.utils.getSingleElement("link", 1);
+            var expected = "item2";
+            assert.equal(actual.id, expected);
+            stub.restore();
+        });
+    });
+    describe("Getting index of an element", function () {
+        it("should return the correct index", function () {
+            var elem     = {id: "abx"};
+            var stub     = sinon.stub(document, "getElementsByTagName").returns([elem, {id: "xab"}]);
+            var actual   = browserUtils.utils.getElementIndex("link", elem);
+            var expected = 0;
+            assert.equal(actual, expected);
+            stub.restore();
+        });
+        it("should return the correct index", function () {
+            var elem     = {id: "xab"};
+            var stub     = sinon.stub(document, "getElementsByTagName").returns([{id: "abx"}, elem]);
+            var actual   = browserUtils.utils.getElementIndex("link", elem);
+            var expected = 1;
+            assert.equal(actual, expected);
+            stub.restore();
+        });
+    });
+
+    describe("Getting element data for socket event", function(){
+        var indexStub;
+        before(function () {
+            indexStub = sinon.stub(browserUtils.utils, "getElementIndex").returns(0);
+        });
+        after(function () {
+            indexStub.restore();
+        });
+        it("should return an object containing element info", function(){
+            var elem = {
+                tagName: "link"
+            };
+            var actual   = browserUtils.utils.getElementData(elem);
+            assert.equal(actual.tagName, "link");
+            assert.equal(actual.index, 0);
+        });
+    });
 });
-
-//    describe("getting a single element", function(){
-//        var fakeElems = [
-//            {
-//                id: "item1",
-//                tagName: "link"
-//            },
-//            {
-//                id: "item2",
-//                tagName: "link"
-//            }
-//        ], stub;
-//        before(function(){
-//            stub = sinon.stub(document, "getElementsByTagName").returns(fakeElems);
-//        });
-//        after(function () {
-//            stub.restore();
-//        });
-//        it("should call getElementsByTagName() with the tagname", function(){
-//            clicks.getSingleElement("link", 0);
-//            sinon.assert.calledWithExactly(stub, "link");
-//        });
-//        it("should return the correct index: 1", function(){
-//            var actual   = clicks.getSingleElement("link", 0);
-//            var expected = "item1";
-//            assert.equal(actual.id, expected);
-//        });
-//        it("should return the correct index: 2", function(){
-//            var actual   = clicks.getSingleElement("link", 1);
-//            var expected = "item2";
-//            assert.equal(actual.id, expected);
-//            stub.restore();
-//        });
-//    });
-
-
-//    describe("Getting index of an element", function () {
-//        it("should return the correct index", function () {
-//            var elem     = {id: "abx"};
-//            var stub     = sinon.stub(document, "getElementsByTagName").returns([elem, {id: "xab"}]);
-//            var actual   = clicks.getElementIndex("link", elem);
-//            var expected = 0;
-//            assert.equal(actual, expected);
-//            stub.restore();
-//        });
-//        it("should return the correct index", function () {
-//            var elem     = {id: "xab"};
-//            var stub     = sinon.stub(document, "getElementsByTagName").returns([{id: "abx"}, elem]);
-//            var actual   = clicks.getElementIndex("link", elem);
-//            var expected = 1;
-//            assert.equal(actual, expected);
-//            stub.restore();
-//        });
-//    });
-
-//    describe("Getting element data for socket event", function(){
-//        var indexStub;
-//        before(function () {
-//            indexStub = sinon.stub(clicks, "getElementIndex").returns(0);
-//        });
-//        after(function () {
-//            indexStub.restore();
-//        });
-//        it("should return an object containing element info", function(){
-//            var elem = {
-//                tagName: "link"
-//            };
-//            var actual   = clicks.getElementData(elem);
-//            assert.equal(actual.tagName, "link");
-//            assert.equal(actual.index, 0);
-//        });
-//    });
