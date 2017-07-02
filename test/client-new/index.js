c86d856aa0e6eb0489e5e738bf601ceb42c13b41
Mateusz Derks <mateusz.derks@toptal.com>
2017-07-02 11:08:14 +0200

    Allow to access browserSync instance from a plugin

--- test/client-new/index.js
+++ test/client-new/index.js
@@ -5,6 +5,7 @@ describe("Init method", function(){
     var ghostMode   = window.__bs_ghost_mode__;
     var bs          = window.__bs_stub__;
     var codeSync    = window.__bs_code_sync__;
+    var BrowserSync = window.__bs;
     var notifySpy;
     var notifyFlashSpy;
     var ghostStub;
@@ -16,17 +17,31 @@ describe("Init method", function(){
         ghostStub       = sinon.stub(ghostMode, "init");
         codeSyncStub    = sinon.stub(codeSync, "init");
     });
+    beforeEach(function () {
+        window.___browserSync___ = {};
+    });
+    afterEach(function (){
+        delete window.___browserSync___;
+    });
     after(function () {
         notifySpy.restore();
         notifyFlashSpy.restore();
         ghostStub.restore();
         codeSyncStub.restore();
     });
-    it("should initilize", function(){
+    it("should initialize", function(){
         index.init(bs.options);
         sinon.assert.called(notifySpy);
         sinon.assert.called(notifyFlashSpy);
         sinon.assert.called(ghostStub);
         sinon.assert.called(codeSyncStub);
+    });
+    it("should expose browserSync instance", function(){
+        var spy = sinon.spy();
+        var BS = window.___browserSync___;
+        index.init(bs.options);
+        assert.equal(typeof BS.use === "function", true);
+        BS.use(spy);
+        sinon.assert.calledWith(spy, sinon.match.instanceOf(BrowserSync));
     });
 });
